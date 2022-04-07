ea5dc992e01542e7f2fb56b2a05312834f78b4a3
hauselin <hauselin@gmail.com>
2022-04-07 14:28:28 -0600

    Fit hierarchical models to fold0

--- src/fit_hddm.R
+++ src/fit_hddm.R
@@ -1,11 +1,15 @@
 #%%
 rm(list = ls())
-library(tidyverse); library(data.table); library(hBayesDM); 
+library(tidyverse); library(data.table); library(hBayesDM); library(parallel)
 theme_set(theme_minimal())
 #%%
 
+cores <- 1  # unused parameter for now
+f <- 0  # which fold 
 dt1 <- fread("../data/clean/data_fold01.csv")
+n_countries <- dt1[, n_distinct(country)]
 dt1[, unique(country)]
+dt1 <- dt1[fold == f]
 
 fit_model <- function(x) {
     # prep data
@@ -15,14 +19,21 @@ fit_model <- function(x) {
     temp_dt[, choice := choice + 1]
     
     # fit model
-    # fit <- choiceRT_ddm(temp_dt, niter = 20, nwarmup = 10, nchain = 4, ncore = 4)
     fit <- choiceRT_ddm(temp_dt, niter = 5000, nwarmup = 1000, nchain = 4, ncore = 4)
     
     # save model
-    output <- paste0("../results/hddm_m0_country", sprintf("%02d", x), ".rds")
+    output <- paste0("../results/fold0_hddm_m0_country", sprintf("%02d", x), ".rds")
     message(paste0("Saving model to ", output))
     write_rds(fit, output)
 }
 
-fit_model(2)
+
+# results <- mclapply(1:n_countries, fit_model, mc.cores = cores)
+for (c in 1:n_countries) {
+    print(paste0("fitting model to country ", c))
+    fit_model(c)
+}
+
+
+
 
